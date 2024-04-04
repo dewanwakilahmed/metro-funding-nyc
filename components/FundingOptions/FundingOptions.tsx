6b9575c04ec920be7efa918a392669ab7523d18e
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-04-05 00:47:51 +0600

    Finished FundingOptions Section

--- components/FundingOptions/FundingOptions.tsx
+++ components/FundingOptions/FundingOptions.tsx
@@ -17,55 +17,31 @@ const FundingOptions = () => {
           <MdAttachMoney className='funding-options-item-icon' />
           <h4 className='funding-options-item-title'>Merchant Cash Advance</h4>
           <p className='funding-options-item-desc'>
-            Get the capital you need now by leveraging your future sales. Ideal
-            for businesses seeking quick, flexible funding without the rigidity
-            of traditional loans.
+            Leverage your future sales to secure the capital you need today with
+            our Merchant Cash Advance, the ideal solution for businesses in
+            search of quick and flexible funding. Enjoy the benefits of fast
+            funding with access to capital within 24 hours of approval, flexible
+            repayment terms tied to daily sales, and the convenience of no
+            collateral requirements. This unsecured funding option is perfect
+            for managing cash flow, especially for businesses experiencing
+            seasonal sales or fluctuating revenue.
           </p>
-          <ul className='funding-options-item-highlights'>
-            <li className='funding-options-item-highlight'>
-              Fast Funding: Access funds within 24 hours of approval.
-            </li>
-            <li className='funding-options-item-highlight'>
-              Flexible Repayment: Repayments are based on a percentage of daily
-              sales.
-            </li>
-            <li className='funding-options-item-highlight'>
-              No Collateral Required: Unsecured funding based on your
-              business&apos;s sales performance.
-            </li>
-            <li className='funding-options-item-highlight'>
-              Great for Cash Flow Management: Perfect for businesses with
-              seasonal sales or fluctuating revenue.
-            </li>
-          </ul>
         </div>
 
         <div className='funding-options-item'>
           <FaRegHandshake className='funding-options-item-icon' />
           <h4 className='funding-options-item-title'>SBA Loan</h4>
           <p className='funding-options-item-desc'>
-            Secure a low-interest loan backed by the Small Business
-            Administration. A great option for businesses planning significant
-            investment or expansion.
+            Secure your business's future with a low-interest SBA Loan, an
+            excellent choice for significant investment or expansion efforts.
+            Benefit from competitive interest rates, among the lowest in the
+            market, and enjoy the flexibility of extended repayment terms—up to
+            10 years for working capital and 25 years for real estate loans.
+            With a wide range of loan amounts available, from $30K to $5M,
+            tailor your borrowing to fit your specific business needs. This loan
+            is ideally suited for businesses aiming for growth and expansion,
+            providing a solid foundation to build upon.
           </p>
-          <ul className='funding-options-item-highlights'>
-            <li className='funding-options-item-highlight'>
-              Competitive Interest Rates: Enjoy some of the lowest rates
-              available on the market.
-            </li>
-            <li className='funding-options-item-highlight'>
-              Longer Repayment Terms: Terms can extend up to 10 years for
-              working capital loans and up to 25 years for real estate loans.
-            </li>
-            <li className='funding-options-item-highlight'>
-              Range of Loan Amounts: Borrow from $30K to $5M to suit your
-              business needs.
-            </li>
-            <li className='funding-options-item-highlight'>
-              Great for Growth and Expansion: Ideal for businesses looking to
-              invest in their future.
-            </li>
-          </ul>
         </div>
       </div>
     </section>
